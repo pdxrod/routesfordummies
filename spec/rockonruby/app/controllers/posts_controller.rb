9d6108c83b394a06799277d52bbc8be626c1499d
Rod McLaughlin <rodmclaughlin@gmail.com>
2012-11-14 14:36:30 -0800

    Still not working - but tests pass - need to use request.GET and request.POST to distinguish POST from GET parameters

--- spec/rockonruby/app/controllers/posts_controller.rb
+++ spec/rockonruby/app/controllers/posts_controller.rb
@@ -3,14 +3,15 @@ class PostsController < ApplicationController
   def post_action # See the integration tests for what this is about -    
                   # POST "posts/foo/bar?a=not_foo" with params {:a => 'still_not_foo'} 
     @array = url2array  
-    @hash = params2hash params                        
+    @hash = params2hash 
     @params = params.dup 
 
 puts "\n array[ 0 ] = #{@array[ 0 ]}, params[ 'a' ] = #{@params[ 'a' ]}, hash[ 'a' ] = #{@hash['a']}\n"
+puts " request.POST = #{request.POST}, request.GET = #{request.GET}\n"
 
     raise "array[ 0 ] should be 'foo' not '#{ @array[ 0 ]}'" unless @array[ 0 ] == 'foo'
-    raise "params[ 'a' ] should be 'not_foo' not '#{ @params[ 'a' ]}'" unless @params[ 'a' ] == 'not_foo'
-    raise "hash[ 'a' ] should be '#{ POSTS_PARAM }' not '#{ @hash[ 'a' ]}'" unless @hash[ 'a' ] == POSTS_PARAM
+    raise "params[ 'a' ] should be 'foo' not '#{ @params[ 'a' ]}'" unless @params[ 'a' ] == 'foo'
+    raise "hash[ 'a' ] should be 'not_foo' not '#{ @hash[ 'a' ]}'" unless @hash[ 'a' ] == 'not_foo'
 
   end       
 
