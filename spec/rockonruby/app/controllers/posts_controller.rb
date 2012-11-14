c876eec9e2bc29cdf7115b009c00b2e0a90bf8fa
Rod McLaughlin <rodmclaughlin@gmail.com>
2012-11-14 15:05:21 -0800

    Tests pass - distinguish POST from GET parameters, and override the latter with the former if they have any keys in common

--- spec/rockonruby/app/controllers/posts_controller.rb
+++ spec/rockonruby/app/controllers/posts_controller.rb
@@ -1,17 +1,20 @@
 class PostsController < ApplicationController
 
-  def post_action # See the integration tests for what this is about -    
+  def post_action # See integration test restful_verb_test for what this is about -    
                   # POST "posts/foo/bar?a=not_foo" with params {:a => 'still_not_foo'} 
     @array = url2array  
     @hash = params2hash 
     @params = params.dup 
 
-puts "\n array[ 0 ] = #{@array[ 0 ]}, params[ 'a' ] = #{@params[ 'a' ]}, hash[ 'a' ] = #{@hash['a']}\n"
-puts " request.POST = #{request.POST}, request.GET = #{request.GET}\n"
+# puts "\n array[ 0 ] = #{@array[ 0 ]}, params[ 'a' ] = #{@params[ 'a' ]}, hash[ 'a' ] = #{@hash['a']}\n"
+# puts " request.POST = #{request.POST}, request.GET = #{request.GET}\n"
 
-    raise "array[ 0 ] should be 'foo' not '#{ @array[ 0 ]}'" unless @array[ 0 ] == 'foo'
-    raise "params[ 'a' ] should be 'foo' not '#{ @params[ 'a' ]}'" unless @params[ 'a' ] == 'foo'
-    raise "hash[ 'a' ] should be 'not_foo' not '#{ POSTS_PARAM }'" unless @hash[ 'a' ] == POSTS_PARAM
+    raise "request.POST[ 'a' ] should be '#{ POSTS_PARAM }'" unless request.POST[ 'a' ] == POSTS_PARAM
+    raise "request.GET[ 'a' ] should not be '#{ POSTS_PARAM }'" if request.GET[ 'a' ] == POSTS_PARAM
+
+    raise "array[ 0 ] should be 'foo' not '#{ @array[ 0 ] }'" unless @array[ 0 ] == 'foo'
+    raise "params[ 'a' ] should be 'foo' not '#{ @params[ 'a' ] }'" unless @params[ 'a' ] == 'foo'
+    raise "hash[ 'a' ] should be '#{ POSTS_PARAM }' not '#{ @hash[ 'a' ] }'" unless @hash[ 'a' ] == POSTS_PARAM
 
   end       
 
