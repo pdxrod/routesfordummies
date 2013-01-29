45b39a5369610cfac9cf5deef818174960e10be7
Rod McLaughlin <rodmclaughlin@gmail.com>
2013-01-29 09:17:05 -0800

    Put timers in get_test to show how long it takes - about seven minutes on a Mac Air 1.6 GHz core i5

--- spec/rockonruby/test/integration/get_test.rb
+++ spec/rockonruby/test/integration/get_test.rb
@@ -5,7 +5,7 @@ class GetTest < ActionDispatch::IntegrationTest
   def test_should_try_to_get_abcdeg_path_with_optional_parameters_if_optional_set
 
     if ENV[ 'OPTIONAL_PARAMETERS' ].to_s.strip != '1'
-      puts "\nSet environment variable OPTIONAL_PARAMETERS to 1 to run test for routes like this: controller/(:a)/(:b)/(:c) etc."
+      puts "\n#{ File.basename $0 }: Set environment variable OPTIONAL_PARAMETERS to 1 to run test for routes like this: controller/(:a)/(:b)/(:c) etc."
     else
 
       Rockonruby::Application.routes.prepend do 
@@ -13,12 +13,20 @@ class GetTest < ActionDispatch::IntegrationTest
       end
       Rockonruby::Application.reload_routes!
 
-      get ABCDEG_PATH
+      puts
+      t = Time.now 
+      puts "Started at #{t}"
+      get ABCDEG_PATH 
+      assert_response :success
+      s = Time.now
+      puts "Finished at #{s}"
+      u = ((s - t) / 60.0).to_s 
+      puts "\n#{ File.basename $0 }: Took #{ u[ 0 .. (u.index( '.' ) + 1) ] } minutes" 
+
     end
 
   end
 
 end
-
 
 
